ef65e44a0d080397cb4f76e1f2f4feab39381046
vinaynathtiwary <vinaynathtiwary@gmail.com>
2025-01-12 20:44:54 +0530

    google tag

--- app/layout.js
+++ app/layout.js
@@ -58,20 +58,7 @@ export default function RootLayout({ children }) {
 
         <title>HackFusion | Website Development Services in India</title>
 
-        {/* Google Tag Manager */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-KBQDJNRZ');
-            `,
-          }}
-        />
-
-        {/* Google Analytics */}
+        {/* Google tag (gtag.js) */}
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-Y40E73HSER"></script>
         <script
           dangerouslySetInnerHTML={{
@@ -79,23 +66,13 @@ export default function RootLayout({ children }) {
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
+
               gtag('config', 'G-Y40E73HSER');
             `,
           }}
         />
       </Head>
       <body className={inter.className}>
-        {/* Google Tag Manager (noscript) */}
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-KBQDJNRZ"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          ></iframe>
-        </noscript>
-        {/* End Google Tag Manager (noscript) */}
-
         {children}
       </body>
     </html>
